0ae6c45f42d3fb9227c4c1f2834464c15f0fd302
SeawardSalmon6 <rayhanachour@hotmail.com>
2021-07-15 23:24:17 -0300

    defining snake movements

--- projects/snake_game_web_basics/script.js
+++ projects/snake_game_web_basics/script.js
@@ -1,11 +1,14 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+
 let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+
+let direction = "right";
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -19,5 +22,26 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100);
